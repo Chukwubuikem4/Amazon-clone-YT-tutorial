103e505ed68086c9a400c2e98cff03946ac160f5
Chukwubuikem4 <emmanuelikwuka1@gmail.com>
2024-10-31 19:29:01 +0100

    deleted products manually created

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,35 +1,3 @@
-const products = [
-  {
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: {
-      stars: 4.5,
-      count: 87
-    },
-    priceCents: 1090
-  },
-
-  {
-    image: "images/products/intermediate-composite-basketball.jpg",
-    name: "Intermediate Size Basketball",
-    rating: {
-      stars: 4,
-      count: 127
-    },
-    priceCents: 2095
-  },
-
-  {
-    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-    name: "Adults Plain Cotton T-Shirt - 2 Pack",
-    rating: {
-      stars: 4.5,
-      count: 56
-    },
-    priceCents: 799
-  }
-];
-
 let productsHTML = "";
 
 products.forEach((product)=>{
@@ -91,17 +59,23 @@ document.querySelectorAll('.add-to-cart-button').forEach(button =>{
   button.addEventListener('click', ()=>{
     const productName = button.dataset.productName;
 
-    let matchingItems;
-    
+    let matchingItem;
+
     cart.forEach(item =>{
       if (productName === item.productName){
-
+        matchingItem = item;
       }
-    })
-    cart.push({
-      productName,
-      quantity: 1
     });
+
+    if(matchingItem){
+      matchingItem.quantity += 1;
+
+    } else{
+      cart.push({
+        productName,
+        quantity: 1,
+      })
+    }
     console.log(cart);
   })
 })
